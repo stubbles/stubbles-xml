1d22bac7ba02638105d46c747c4145a11a0e4037
Frank Kleine <fkleine@united-internet.de>
2019-11-12 15:32:38 +0100

    remove useless @package declaration

--- src/main/php/XmlStreamWriterProvider.php
+++ src/main/php/XmlStreamWriterProvider.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * @package  stubbles\xml
  */
 namespace stubbles\xml;
 use stubbles\ioc\InjectionProvider;
